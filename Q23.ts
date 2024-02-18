f1f77c82dd6d2c1ba06228830fce49a85600b6d3
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-18 19:03:03 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,58 @@
+// 23.Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+//(5 question of true)
+
+
+// Test 1
+let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I prdict True Otherwise I predict False")
+
+console.log(car == 'subaru')
+console.log(car == 'corolla')
+
+// Test 2
+let num: any = 10
+
+// console.log("Is num == 10(in number) ? I prdict true otherwise I predict false")
+console.log(num === 10 )
+console.log(num === "10")
+
+//Test 3
+
+let Name: string = "junaid"
+
+// console.log("Is Name == junaid ? I prdict true otherwise I predict false")
+
+console.log(Name === "junaid")
+console.log(Name === "kamran" )
+
+//Test 3
+let fruit: string = "Banana"
+
+// console.log("Is fruit == Banana ? I prdict true otherwise I predict false")
+
+console.log(fruit == "Banana")
+console.log(fruit == "Apple" )
+
+//Test 4
+let dish : string = "Biryani"
+
+// console.log("Is dish == karhai gosht ? I prdict true otherwise I predict false")
+
+console.log(dish == "karhai gosht")
+console.log(dish == "beef palao")
+
+//Test 5
+let dress : string = "Hoodies"
+
+//console.log("Is dress == Hoodies ? I predict true otherwise I predict False")
+
+console.log(dress == "Hoodies")
+console .log(dress == "shirts")
+
